18cea270786570e36d91fd302ed201efaed9b40a
Marcos Caballero Fernández <marcowerjr@gmail.com>
2022-02-17 22:35:02 -0700

    Finish the Ping Pong Scoreboard with a simple UI

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv/config');
 
-//settings
 const PORT = process.env.PORT || 3000;
 var corsOptions = {
     origin: '*',
@@ -13,17 +12,14 @@ var corsOptions = {
     allowedHeaders: '*'
 }
 
-//headers
 app.use(cors(corsOptions));
 
-//middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
+app.set('view engine', 'ejs');
 
-//Routes
 app.use(require('./src/app.js'));
 
-//Server Start
 console.clear();
 app.listen(PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on localhost port ${PORT}`)
--- src/app.js
+++ src/app.js
@@ -2,5 +2,22 @@ const express = require('express');
 const mysqlConnection = require('./database.js');
 const app = express.Router();
 
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/continue', (req, res) => {
+    res.render('continue', { players: { player1_name: req.body.player1_name, player2_name: req.body.player2_name } });
+});
+
+app.post('/save_game', (req, res) => {
+    mysqlConnection.query('INSERT INTO game_history SET ?', [req.body], (err) => {
+        if (!err) {
+            return res.json({ 'message': 'success' });
+        } else {
+            console.log(err);
+        }
+    });
+});
 
 module.exports = app;
